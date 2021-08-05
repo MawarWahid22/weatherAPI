6cf1802ae4b7d3f7e3ed3025b396c631343bbd56
MawarWahid22 <84270936+MawarWahid22@users.noreply.github.com>
2021-08-05 15:46:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -40,16 +40,10 @@ app.get('/weather/:latlon', async (request, response) => {
   const lon = latlon[1];
   console.log(lat, lon);
   const api_key = process.env.API_KEY;
-  //const weather_url = `https://api.aerisapi.com/observations/${nxlpgAJJ24XQj6T9fHc8dq7RR4JhFceEkNebkaSc}/${lat},${lon}/?units=si`;
-  //const weather_url = 'https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=GIFfh3254CFFcPveWcXzpyp3azZpEE8u';
-  
+
  const weather_url = 'https://api.tomorrow.io/v4/timelines?lat=${latitude}&lon=${longitude}&fields%5B%5D=temp&fields%5B%5D=weather_code&fields%5B%5D=feels_like&fields%5B%5D=precipitation_type&apikey=${GIFfh3254CFFcPveWcXzpyp3azZpEE8u}';
   
-  //const weather_url = 'https://api.aerisapi.com/airquality/minneapolis,mn?format=json&client_id=[7B4FJi7FiQADI95IonrlN]&client_secret=[nxlpgAJJ24XQj6T9fHc8dq7RR4JhFceEkNebkaSc]';
-  //const weather_url = 'https://api.aerisapi.com/airquality/minneapolis,mn?format=json&fields=loc&client_id=[7B4FJi7FiQADI95IonrlN]&client_secret=[nxlpgAJJ24XQj6T9fHc8dq7RR4JhFceEkNebkaSc]';
-
-  //const weather_url = 'weather/${lat},${lon}';
-
+ 
   const weather_response = await fetch(weather_url);
   const weather_data = await weather_response.json();
 
